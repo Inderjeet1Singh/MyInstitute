1f76198fb29779a525f3018d4186377cb7054884
Inderjeet1Singh <inderjeet2023mnnit@gmail.com>
2025-06-20 18:39:29 +0530

    frontend setup

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <div className="flex gap-2 bg-gray-600 h-auto w-full">
+      <div>
+        <img src="" alt="Logo" />
+      </div>
+      <Link>Home</Link>
+      <Link></Link>
+    </div>
+  );
+};
+
+export default Navbar;
